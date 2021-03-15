45d199ad3b1df1fa5dab744e3d1ef59078e0eb27
Matthew Elliott <77035998+MatteoThomas@users.noreply.github.com>
2021-03-14 23:44:18 -0600

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-04 Homework - Quiz
+ ## Code Quiz Homework
+I didn't get to finish unfortunately but I did spend more time on my readme.
+
+## Known Issues
+*  `Answer buttons` will only show the 4th value of whichever question it is on. Will not advance the question. Will not add or subtract time from the timer.
+* ~~`Start button` has to be pressed twice to work. I think it's because of the event listener.~~
+*  `Timer` is not near the other elements.
+*  `Scoring` Did not start.
+*  `Timer` doesn't show at the highest value. I faked it and put a 7 in the html div, then the timer replaces it when it get to 6.
+
+## Links
+[Github Repository](https://github.com/MatteoThomas/04-homework-code-quiz)
+
+[Code Quiz Homework](https://matteothomas.github.io/04-homework-code-quiz/)
+
+## Preview
+![screenshot](assets/img/Code_Quiz_Homework_Screenshot.png)
---
+++ assets/js/script.js
@@ -0,0 +1,182 @@
+var timerElement = document.querySelector("#timer-count");
+var questionEl = document.querySelector("#questions");
+var resetButton = document.querySelector("reset-button");
+var answerButton = document.querySelector("#ab");
+var startButton = document.querySelector("#start-button");
+var answerEl = document.querySelector("#answerSection");
+var timer;
+var timerCount = 7;
+
+window.onload = hideTimer();
+hideReset();
+
+var pr = 'Press button to play';
+var el = document.getElementById('questions');
+el.textContent = pr;
+
+function reset() {
+  hideReset();
+  showStart();
+  hideTimer();
+  reload();
+
+}
+
+function reload() {
+  location.reload();
+
+}
+
+function hideTimer() {
+  document.getElementById("timer-count").style.display = 'none';
+}
+
+function showTimer() {
+  document.getElementById("timer-count").style.display = 'initial';
+}
+
+function hideReset() {
+  document.getElementById("reset-button").style.display = 'none';
+}
+
+function showReset() {
+  document.getElementById("reset-button").style.display = 'initial';
+}
+
+function hideStart() {
+  document.getElementById("start-button").style.display = 'none';
+}
+
+function showStart() {
+  document.getElementById("start-button").style.display = 'initial';
+}
+
+function gameOver() {
+  console.log("Game over");
+  var overMessage = "Your score here";
+  var el = document.getElementById('questions');
+  el.textContent = overMessage;
+  showReset();
+  document.getElementById('answerSection').style.display = 'none';
+
+}
+
+function startTimer() {
+  var timer = setInterval(function () {
+    timerCount--;
+    timerElement.textContent = timerCount;
+
+    if (timerCount === 0) {
+      timerElement.textContent = 'Game over';
+    }
+    if (timerCount === 0) {
+      clearInterval(timer);
+      gameOver();
+    }
+
+  }, 1000);
+
+}
+
+var index = 0;
+var questions = [{
+    question: "Which is not an Error Name value?",
+    choices: ['Type Error', 'Syntax Error', 'Main Error', 'Reference Error'],
+    correct: 'Main Error'
+  },
+
+  {
+    question: "What does Boolean return?",
+    choices: ['True/ False', '1/2', 'Yes / No', 'Correct / Missing'],
+    correct: 'True / False'
+  },
+
+  {
+    question: "How many values can be stored in an array",
+    choices: ['1', '2', '3', '4'],
+    correct: '4'
+  },
+
+  {
+    question: "What type of value does a string contain?",
+    choices: ['Numbers', 'Text', 'Boolean', 'JSON'],
+    correct: 'Text'
+  }
+]
+
+
+
+let shuffleQuestions, currentQuestionIndex
+
+function startGame() {
+
+  document.getElementById("start-button").style.display = 'none';
+  document.getElementById('answerSection').style.display = 'initial';
+  shuffleQuestions = questions.sort(() => Math.random() - .5);
+  currentQuestionIndex = 0;
+
+  startTimer();
+  showTimer();
+  showQuestion(shuffleQuestions[currentQuestionIndex]);
+  nextQuestion();
+
+
+}
+
+function showQuestion() {
+  var currentQuestion = questions[currentQuestionIndex]
+
+  questionEl.innerText = currentQuestion.question
+
+  currentQuestion.choices.forEach(element => {
+    var button = document.createElement('button')
+    console.log(button)
+    button.classList.add('ab')
+    answerButton.innerText = element
+    answerButton.appendChild(button)
+    button.addEventListener('click', selectAnswers)
+
+  })
+}
+
+
+
+function resetQuestion() {
+  while (answerButton.firstChild) {
+    answerButton.removeChild(answerButton.firstChild)
+
+  }
+}
+
+function nextQuestion() {
+  resetQuestion()
+  showQuestion(shuffleQuestions[currentQuestionIndex])
+}
+
+function selectAnswers() {
+  if (this.textContent !== questions[currentQuestionIndex])
+    timerCount -= 5;
+  if (timerCount < 0) {
+    timerCount = 0;
+  } else {
+    timerCount += 5;
+  }
+  timer.textContent = timerCount;
+}
+
+document.getElementById('answerSection').style.display = 'none';
+document.getElementById('start-button').addEventListener('click', startGame);
+document.getElementById('reset-button').addEventListener('click', reset);
+//document.getElementById('answerSection').addEventListener('click', nextQuestion);
+
+
+
+// dominic timer code
+/* if (this.textContent !== questions[currentQuestionIndex].correct) {
+  timerCount -= 35;
+  if (timerCount < 0) {
+      timerCount = 0;
+  }
+  timer.textContent = timerCount;
+
+  */
